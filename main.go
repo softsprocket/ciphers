a6eeedbe664d5fd1387dd2ea12c8c778f13de0cd
Greg Martin <greg@softsprocket.com>
2019-05-17 12:45:06 -0700

    commands

--- main.go
+++ main.go
@@ -43,13 +43,57 @@ var usageStrings = map[string]string{
 	"app":            "usage: %s <command> <args>",
 	"help":           "usage: %s help <command>\ncommands: gen-keys encrypt-string decrypt-string",
 	"gen-keys":       "usage: %s gen-keys --private <filename> --public <filename>",
-	"encrypt-string": "usage: %s encrypt-string <string>",
-	"decrypt-string": "usage: %s decrypt-string <string>",
+	"encrypt-string": "usage: %s encrypt-string --public <filename> --in <filename> --out <filename>",
+	"decrypt-string": "usage: %s decrypt-string --private <filename> --in <filename> --out <filename>",
 }
 
 func printUsageAndExit(command string, exitValue int) {
 	fmt.Printf(usageStrings[command], filepath.Base(os.Args[0]))
 	os.Exit(exitValue)
+}
+
+func parseArguments(arguments []string) map[string]string {
+	var args = arguments[2:]
+	argMap := make(map[string]string)
+
+	for i := 0; i < len(args); i++ {
+		runes := []rune(args[i])
+		key := string(runes[2:])
+		i++
+		argMap[key] = args[i]
+	}
+
+	return argMap
+}
+
+func getKeyFileNames(command string, arguments []string) map[string]string {
+	files := parseArguments(arguments)
+
+	ok := false
+
+	if command == "gen-keys" {
+		if _, ok = files["private"]; ok {
+			_, ok = files["public"]
+		}
+	} else {
+		if command == "encrypt-string" {
+			_, ok = files["public"]
+		} else if command == "decrypt-string" {
+			_, ok = files["private"]
+		}
+
+		if ok {
+			if _, ok = files["in"]; ok {
+				_, ok = files["out"]
+			}
+		}
+	}
+	if !ok {
+		printUsageAndExit(command, -1)
+	}
+
+	return files
+
 }
 
 func main() {
@@ -74,30 +118,46 @@ func main() {
 			printUsageAndExit("gen-keys", -1)
 		}
 
-		files := make(map[string]string)
-		runes := []rune(arguments[2])
-		first := string(runes[2:])
-		runes = []rune(arguments[4])
-		second := string(runes[2:])
-
-		files[first] = arguments[3]
-		files[second] = arguments[5]
-
-		_, privOk := files["private"]
-		_, pubOk := files["public"]
-		if !privOk || !pubOk {
-			printUsageAndExit("gen-keys", -1)
-		}
+		files := getKeyFileNames("gen-keys", arguments)
 
 		genKeyPair(files["private"], files["public"])
 
 	case "encrypt-string":
-		if len(arguments) < 3 {
+		if len(arguments) < 8 {
 			printUsageAndExit("encrypt-string", -1)
 		}
+
+		files := getKeyFileNames("encrypt-string", arguments)
+
+		publicKey, err := ioutil.ReadFile(files["public"])
+
+		if err != nil {
+
+		}
+
+		toEncrypt, err := ioutil.ReadFile(files["in"])
+
+		encrypted := encryptString(string(toEncrypt), publicKey)
+
+		ioutil.WriteFile(files["out"], encrypted, 0644)
+
 	case "decrypt-string":
-		if len(arguments) < 3 {
-			printUsageAndExit("encrypt-string", -1)
+		if len(arguments) < 8 {
+			printUsageAndExit("decrypt-string", -1)
 		}
+
+		files := getKeyFileNames("decrypt-string", arguments)
+
+		privateKey, err := ioutil.ReadFile(files["private"])
+
+		if err != nil {
+
+		}
+
+		toDecrypt, err := ioutil.ReadFile(files["in"])
+
+		decrypted := decryptToString(toDecrypt, privateKey)
+
+		ioutil.WriteFile(files["out"], []byte(decrypted), 0644)
 	}
 }
